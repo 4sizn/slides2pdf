7101303980f8062c4664c3bd26a68d33d05e9cf3
4sizn <4sizn@naver.com>
2020-07-18 23:21:46 +0900

    no message

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 const puppeteer = require("puppeteer");
+const fs = require("fs-extra");
 const FULLSCREEN_SELECTOR = `a.fullscreen-button`;
+const html =
+  "https://infinite-red.slides.com/gantlaborde/adventures-in-ai-javascript#/4";
 
 (async () => {
+  init();
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(
-    "https://infinite-red.slides.com/gantlaborde/adventures-in-ai-javascript#/4"
-  );
-
-  await page.click(FULLSCREEN_SELECTOR);
+  await page.goto(html);
+  await page.evaluate((sel) => {
+    document.querySelector(sel).click();
+  }, FULLSCREEN_SELECTOR);
+  // await page.click(FULLSCREEN_SELECTOR);
   // await page.waitFor(3000);
-  await page.keyboard.press("ArrowRight");
-  await page.keyboard.press("ArrowDown");
 
-  await page.screenshot({ path: "example.png" });
+  let current = html;
+  let n = 0;
+  do {
+    await page.screenshot({ path: `a/example${++n}.png` });
+    current = page.url();
+    await page.keyboard.press("ArrowDown");
+  } while (current !== page.url());
 
   // await page.pdf({ path: "hn.pdf", format: "A4" });
   await browser.close();
 })();
+
+async function init() {
+  await fs.remove("a");
+  await fs.promises.mkdir("a");
+}
