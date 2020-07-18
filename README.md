95f18f3f9b6017c12da49ebf5f5b88b6480d912f
4sizn <4sizn@naver.com>
2020-07-19 03:34:34 +0900

    feat : update logic

--- README.md
+++ README.md
@@ -1,14 +1,10 @@
-https://github.com/westmonroe/pdf-puppeteer/blob/master/src/convertHTMLToPDF.js
-
-1. 선언 html, callback, (slides옵션, 퍼페티어 옵션)
-
-slidesToPDF()
-
-animation delay
-
-mode : 'slow' | "fast"
+mode : 'slow' | "fast"(tobe)
 
 slow모드는 브라우저 한단계씩 스샷 1. url로 스냅샷 확인
 
-fast모드는 엘리먼트 사전 수집 후, thread polling으로 한번에
-document.querySelector('.stack')으로 슬라이드 적용
+tobe : fast모드는 엘리먼트 사전 수집 가능(document.querySelector('.stack')으로 슬라이드 수집(예외처리 적용해야함)), 각 slides간 데이터 수집 및 병렬작업 처리
+
+TODO
+
+- 테스트 코드
+-
--- index.js
+++ index.js
@@ -1,33 +1,72 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs-extra");
-const FULLSCREEN_SELECTOR = `a.fullscreen-button`;
+const easyimg = require("easyimage");
+const MAIN_SELECTOR = ".reveal-frame";
+const DECK_CUDOS_SELECTOR = ".deck-kudos";
+const CONTROLS_SELECTOR = "aside.controls";
+
+const viewPort = { width: 1280, height: 800 };
+
 const html =
-  "https://infinite-red.slides.com/gantlaborde/adventures-in-ai-javascript#/4";
+  "https://infinite-red.slides.com/gantlaborde/adventures-in-ai-javascript#/";
 
 (async () => {
+  //set local directory before launch
   init();
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(html);
-  await page.evaluate((sel) => {
-    document.querySelector(sel).click();
-  }, FULLSCREEN_SELECTOR);
-  // await page.click(FULLSCREEN_SELECTOR);
-  // await page.waitFor(3000);
 
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.setViewport(viewPort);
+  await page.goto(html);
+
+  //set options
+  const [width, height] = await settings(page);
   let current = html;
   let n = 0;
-  do {
-    await page.screenshot({ path: `output/example${++n}.png` });
-    current = page.url();
-    await page.keyboard.press("ArrowDown");
-  } while (current !== page.url());
 
-  // await page.pdf({ path: "hn.pdf", format: "A4" });
+  do {
+    do {
+      await page.screenshot({
+        path: `output/example${++n}.png`,
+        clip: {
+          x: 0,
+          y: 0,
+          width,
+          height,
+        },
+      });
+      current = page.url();
+      await page.keyboard.press("ArrowDown");
+      await page.waitFor(1000);
+    } while (current !== page.url());
+    current = page.url();
+    await page.keyboard.press("ArrowRight");
+    await page.waitFor(1000);
+  } while (current !== page.url());
   await browser.close();
+
+  await easyimg.convert({
+    src: "output" + "/*." + "png",
+    dst: "result" + ".pdf",
+  });
 })();
 
 async function init() {
   await fs.remove("output");
   await fs.promises.mkdir("output");
+}
+
+async function settings(page) {
+  return await page.evaluate(
+    ([a, b, c]) => {
+      const controlEl = document.querySelector(b);
+      controlEl.parentNode.removeChild(controlEl);
+      const deckEl = document.querySelector(c);
+      deckEl.parentNode.removeChild(deckEl);
+      const mainEl = document.querySelector(a);
+
+      return [mainEl.clientWidth, mainEl.clientHeight];
+    },
+    [MAIN_SELECTOR, DECK_CUDOS_SELECTOR, CONTROLS_SELECTOR]
+  );
 }
